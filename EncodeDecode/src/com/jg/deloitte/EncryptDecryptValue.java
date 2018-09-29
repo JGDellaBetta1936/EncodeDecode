f9c2ec47f5a7fd2930da7929ea91fb3936b39837
John Gold <john_gold@comcast.net>
2018-09-29 15:29:02 -0400

    changes

--- EncodeDecode/src/com/jg/deloitte/EncryptDecryptValue.java
+++ EncodeDecode/src/com/jg/deloitte/EncryptDecryptValue.java
@@ -5,12 +5,6 @@ import org.apache.log4j.Logger;
 public class EncryptDecryptValue {
 	static Logger log = Logger.getLogger(EncryptDecryptValue.class.getName());
 
-	public static void main (String [] args) {
-		EncryptDecryptValue edv1 = new EncryptDecryptValue();
-		log.info("recived back->" + edv1.encodeTheValue("Amin") );
-	}
-	
-	
 	public String encodeTheValue(String inputValue) {
 		log.info("Input value->" + inputValue);
 		StringBuilder sb = new StringBuilder();
@@ -18,26 +12,42 @@ public class EncryptDecryptValue {
 		String secondPart = null;
 		// determine odd or even length
 		if (inputValue.length() % 2 == 0) {
-			log.info("Even length");
+			log.info("E/C Even length");
 			firstPart = inputValue.substring(0, inputValue.length() / 2);
-			secondPart = inputValue.substring(inputValue.length() / 2, inputValue.length()) ;
+			secondPart = inputValue.substring(inputValue.length() / 2, inputValue.length());
 			sb.append(secondPart);
 			sb.append(firstPart);
 		} else {
-			log.info("Odd length");
+			log.info("E/C Odd length");
+			firstPart = inputValue.substring(0, inputValue.length() / 2 + 1);
+			secondPart = inputValue.substring(inputValue.length() / 2 + 1);
+			sb.append(firstPart);
+			sb.append(secondPart);
+			sb.append("#");
 		}
 
 		return sb.toString();
 	}
+
 	public String decodeTheValue(String inputValue) {
-		String rtnString = null;
+		String firstPart = null;
+		String secondPart = null;
+		StringBuilder sb = new StringBuilder();
 		// determine odd or even length
 		if (inputValue.length() % 2 == 0) {
-			log.info("Even length");
+			log.info("D/C Even length");
+			firstPart = inputValue.substring(0, inputValue.length() / 2);
+			secondPart = inputValue.substring(inputValue.length() / 2, inputValue.length());
+			sb.append(secondPart);
+			sb.append(firstPart);
 		} else {
-			log.info("Odd length");
+			log.info("D/C Odd length");
+			firstPart = inputValue.substring(0, inputValue.length() / 2 + 1);
+			secondPart = inputValue.substring(inputValue.length() / 2 + 1);
+			sb.append(firstPart);
+			sb.append(secondPart);
 		}
-
-		return rtnString;
+		log.debug("StringBuilder->" + sb.toString());
+		return sb.toString();
 	}
 }
